cd3a34596aeb75c6789b2de0cdc53630f30f176c
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2021-10-10 11:39:33 +0100

    Done with pause/play and thier websockets

--- websocket/server.js
+++ websocket/server.js
@@ -15,12 +15,23 @@ io.on('connection', (client) => {
     console.log(`Disconnected => ${client.id}`);
   });
   //  create or join a room event from frontend
-  client.on('createRoom', (room, userJoining) => {
+  client.on('createRoom', (room) => {
     client.join(room);
-    console.log(`${userJoining} joined room => ${room}`);
+    console.log(`New user joined room => ${room}`);
     io.to(room).emit('createRoomStatus', `Only users in Room => ${room} can see this message`);
   });
 
+    // function to listen to play and emit events in room
+    client.on('play', (room) => {
+      console.log(`play video on all devices in room => ${room}`);
+      io.to(room).emit('playing', `Playing video in Room => ${room}`);
+    });
+
+    // function to listen to pause and emit events in room
+    client.on('paused', (room) => {
+      console.log(`paused video on all devices in room => ${room}`);
+      io.to(room).emit('paused', `Paused video in Room => ${room}`);
+    });
 
 
 });
